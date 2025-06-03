faf8d5e47b7dc27e788892177ff5d54ebab3d85b
marloweber <weber230@purdue.edu>
2025-06-03 08:27:44 -0500

    made project cards with logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { DM_Serif_Text, DM_Sans } from "next/font/google"
-import { Mail, Linkedin, Github, ChevronDown } from "lucide-react";
+import { Mail, Linkedin, Github, ChevronDown, FileText } from "lucide-react";
 
 const DMSerif = DM_Serif_Text({
   subsets: ["latin"],
@@ -15,22 +15,9 @@ const DMSans = DM_Sans({
 export default function Home() {
   return (
     <div>
-    <div className="h-screen bg-white border-64 border-[#06402B]">
-      {/* Header */}
-      {/* <div className="flex items-center justify-between bg-[#06402B]">
-        <div className={`flex p-10 text-5xl text-white ${DMSerif.className}`}>
-          MW
-        </div>
-        <div className="p-10 gap-10 items-center">
-        <div className={`flex items-center justify-between gap-8 text-white text-2xl ${DMSerif.className}`}>
-          <a href="#">Projects</a>
-          <a href="#">Skills</a>
-        </div>
-      </div>
-      </div>  */}
-
-      {/* Image */}
-      <div className="flex pl-24 pt-48">
+    <div className="h-screen bg-white border-[64px] border-[#06402B] flex items-center justify-center">
+      <div className="flex">
+        {/* Image */}
         <div className="flex flex-col items-center">
           <div className="w-72 h-72 rounded-full overflow-hidden relative border-[5px] border-[#06402B] shadow-lg">
             <Image 
@@ -43,7 +30,7 @@ export default function Home() {
           </div>
         </div>
 
-        {/* Name */}
+        {/* Name and description */}
         <div className="pt-10 pb-10 pr-20 pl-20 items-center">
           <div className={`flex items-center justify-between gap-10 text-[#06402B] text-5xl mt-2 ${DMSerif.className}`}>
             Marlo Weber
@@ -61,16 +48,44 @@ export default function Home() {
             <a href="https://github.com/marloweber" target="_blank" rel="noopener noreferrer">
               <Github className="w-8 h-8 transition-colors" />
             </a>
+            <a href="" target="_blank" rel="noopener noreferrer">
+              <FileText className="w-8 h-8 transition-colors" />
+            </a> 
+            {/* link my resume */}
           </div>
         </div>
       </div>
-    </div> 
+    </div>
     
     
     <div className="bg-white h-screen">
       <div className={`p-[64px] text-[#06402B] text-5xl ${DMSerif.className}`}>
         Projects
       </div>
+      <div className="pl-[64px] flex flex-row gap-[64px]">
+        <div className="h-63 w-112 bg-[#30858c] shadow-lg flex items-center justify-center">
+          <Image 
+              src="/oCEANIC_text_logo_white.png" 
+              alt="Project oCEANIC Logo" 
+              width={400} 
+              height={400}
+              className="object-contain"
+            />
+        </div>
+        <div className="ph-63 w-112 bg-white shadow-lg flex items-center justify-center">
+          <Image 
+              src="/colabifylogo.png" 
+              alt="CoLABify Logo" 
+              width={400} 
+              height={400}
+              className="object-contain"
+            />
+        </div>
+      </div>
+      <div className={`p-[64px] text-[#06402B] text-5xl ${DMSerif.className}`}>
+        Skills
+      </div>
+      
   </div>
 
   </div>
