594b39fea489f4f67351357824e9b02b69eebcaf
marloweber <weber230@purdue.edu>
2025-06-11 20:06:18 -0500

    project modals designed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Marlo Weber",
   icons: {
-    icon: "favicon.ico",
+    icon: "mw-logo.svg",
   },
 };
 
---
+++ src/app/oceanic.ts
@@ -0,0 +1,11 @@
+export const oCEANICText = {
+        blurb: "The Project oCEANIC research team has developed an agent-based computational model of ancient seafaring in Pacific Island communities. The project combines Indigenous and local peoples’ environmental knowledge (ILEK) from communities in the Marshall Islands, Federated States of Micronesia (FSM), and Hawai’i with quantitative computer models of archaeological and paleoclimate data. I currently serve as the Programming Team Lead.",
+        tools: ["Python", "Next.js", "Typescript", "Tailwind CSS"],
+} as const;
+
+export const oCEANICImages = [
+    {
+        path: "",
+        caption: "",
+    }
+] as const;
